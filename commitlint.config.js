ec7be745da3854801d1e00ced9cd057af1a1a60b
Mateuzs <mateusz.zembol@gmail.com>
2021-10-18 10:50:57 +0200

    feat(MEMBERSHIP-777): update release options

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   plugins: ["commitlint-plugin-function-rules"],
   rules: {
+    "body-max-line-length": [0],
     "type-enum": [
       2,
       "always",
